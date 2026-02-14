1f2deb93473eb03dc6ba1390cb41d9a921679a93
nonvegetable <vpalande81203@outlook.com>
2026-02-14 13:17:51 +0530

    initialised backend and frontend

---
+++ asseroo-backend/src/main/java/com/example/asseroo_backend/AsserooBackendApplication.java
@@ -0,0 +1,26 @@
+package com.example.asseroo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AsserooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsserooBackendApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home(){
+		return "lol";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ asseroo-backend/src/test/java/com/example/asseroo_backend/AsserooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asseroo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsserooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
