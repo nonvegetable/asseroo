1bc137e9053162eda9a6263781a00f219544c11b
nonvegetable <vpalande81203@outlook.com>
2026-02-23 12:44:08 +0530

    turned into a spring bean

--- asseroo-backend/src/main/java/com/example/asseroo_backend/blockchain/AsserooChain.java
+++ asseroo-backend/src/main/java/com/example/asseroo_backend/blockchain/AsserooChain.java
@@ -1,52 +1,28 @@
 package com.example.asseroo_backend.blockchain;
-import java.util.ArrayList;
-import com.google.gson.GsonBuilder;
 
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
+
+import java.util.ArrayList;
+
+@Component
 public class AsserooChain {
 
-	public static ArrayList<Block> chain = new ArrayList<Block>();
+    public static ArrayList<Block> chain = new ArrayList<>();
 
-	public static void main(String[] args) {
-		
-		chain.add(new Block("100 coins transferred","0","System","Vighnesh"));
-		// System.out.println("Hash for block 1 : " + genesisBlock.hash);
-		
-		chain.add(new Block("25 coins transferred", chain.get(chain.size() - 1).hash,"Vighnesh", "Satvik"));
-		// System.out.println("Hash for block 2 : " + secondBlock.hash);
-		
-		chain.add(new Block("100 coins transferred", chain.get(chain.size() - 1).hash,"Satvik", "Kavish"));
-		// System.out.println("Hash for block 3 : " + thirdBlock.hash);
+    @PostConstruct
+    public void init() {
 
-		String chainJson = new GsonBuilder().setPrettyPrinting().create().toJson(chain);		
-		System.out.println(chainJson);
+        chain.add(new Block("100 coins transferred","0","System","Vighnesh"));
+        chain.add(new Block("25 coins transferred", chain.get(chain.size() - 1).hash,"Vighnesh", "Satvik"));
+        chain.add(new Block("100 coins transferred", chain.get(chain.size() - 1).hash,"Satvik", "Kavish"));
 
-		
-	}
+        System.out.println("========== BLOCKCHAIN ==========");
 
-
-	public static Boolean isValid(){
-		Block currentBlock;
-		Block previousBlock;
-
-		for(int i = 1; i < chain.size(); i++){
-			currentBlock = chain.get(i);
-			previousBlock = chain.get(i - 1);
-
-			// if(currentBlock.previousHash != previousBlock.previousHash){
-			// 	return false;
-			// }
-
-			if(!currentBlock.hash.equals(currentBlock.calculateHash())){
-				System.out.println("Current Hashes not equal");	
-				return false;
-			}
-
-			if(!previousBlock.hash.equals(currentBlock.previousHash)){
-				System.out.println("Previous Hashes not equal");	
-				return false;
-			}
-		}
-
-		return true;
-	}
+        for (Block block : chain) {
+            System.out.println("Hash: " + block.hash);
+            System.out.println("Previous: " + block.previousHash);
+            System.out.println("--------------------------------");
+        }
+    }
 }
